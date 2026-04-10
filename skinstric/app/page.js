ce7d278c0308928be2b91e38cd16a5d1de415fb5
Kamal <kamal.raghe33@gmail.com>
2026-04-10 16:50:08 -0400

    first commit

---
+++ skinstric/app/page.js
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="center border">
+      <div>Sophisticated</div>
+      <div>Skincare</div>
+    </div>
+  );
+}
