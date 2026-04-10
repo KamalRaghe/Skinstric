7d2213e9bbe9576e8da1e54b24e13f05724e04de
Kamal <kamal.raghe33@gmail.com>
2026-04-10 17:41:31 -0400

    first commit

--- skinstric/app/page.js
+++ skinstric/app/page.js
@@ -5,6 +5,7 @@ export default function Home() {
     <>
     <div className="center" style={{height:"5vh",justifyContent:"space-between"}} >
       <div style={{margin:"15px"}}>SKINSTRIC <span style={{color:"grey"}} >[ INTRO ]</span></div>
+      <div><button style={{backgroundColor:"black",color:'white',padding:"10px",marginTop:"15px",font:"8px"}} >Enter Code</button></div>
     </div>
       <div className="center heading" style={{height:"90vh",justifyContent:"space-between"}}>
         <button>Discover A.I.</button>
