070dfe6aef2cd03adff0c0aebd556ca525d9423f
Kamal <kamal.raghe33@gmail.com>
2026-04-10 17:48:59 -0400

    first commit

--- skinstric/app/page.js
+++ skinstric/app/page.js
@@ -3,11 +3,11 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="border">
-    <div className="center" style={{height:"5vh",justifyContent:"space-between"}} >
+    <div className="center" style={{justifyContent:"space-between"}} >
       <div style={{margin:"15px"}}>SKINSTRIC <span style={{color:"grey"}} >[ INTRO ]</span></div>
       <div><button style={{backgroundColor:"black",color:'white',padding:"10px",marginTop:"15px",fontSize:"12px"}} >Enter Code</button></div>
     </div>
-      <div className="center heading" style={{height:"90vh",justifyContent:"space-between"}}>
+      <div className="center heading" style={{justifyContent:"space-between"}}>
         <button>Discover A.I.</button>
         <div>
           <div>Sophisticated</div>
@@ -15,7 +15,7 @@ export default function Home() {
         </div>
         <button>Take test</button>
       </div>
-      <div className="center" style={{height:"5vh",justifyContent:"start", width:"316px",height:"72px"}} >
+      <div className="center" style={{justifyContent:"start", width:"316px",height:"72px"}} >
         SKINSTRIC DEVELOPED AN A.I. THAT CREATES A HIGHLY-PERSONALIZED ROUTINE TAILORED TO WHAT YOUR SKIN NEEDS.
       </div>
     </div>
