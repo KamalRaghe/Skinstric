38adb601152e39410a69ac6db6ec14ab92924a31
Kamal <kamal.raghe33@gmail.com>
2026-04-10 17:52:27 -0400

    first commit

--- skinstric/app/page.js
+++ skinstric/app/page.js
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="border" style={{height:"100vh",padding:"0",margin:"0px"}} >
+    <div style={{height:"100vh",padding:"0",margin:"0px"}} >
     <div className="center" style={{justifyContent:"space-between"}} >
       <div style={{margin:"15px"}}>SKINSTRIC <span style={{color:"grey"}} >[ INTRO ]</span></div>
       <div><button style={{backgroundColor:"black",color:'white',padding:"10px",marginTop:"15px",fontSize:"12px"}} >Enter Code</button></div>
