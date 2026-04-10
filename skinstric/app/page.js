12a30643ab9957e0d4f50969099465fa6860b0c2
Kamal <kamal.raghe33@gmail.com>
2026-04-10 17:31:18 -0400

    first commit

--- skinstric/app/page.js
+++ skinstric/app/page.js
@@ -3,10 +3,16 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-      <div className="center heading border" style={{height:"100vh",justifyContent:"space-between"}}>
-        <div>Sophisticated</div>
-        <div>Skincare</div>
+    <div className="center" style={{height:"5vh",justifyContent:"space-between"}} ></div>
+      <div className="center heading" style={{height:"90vh",justifyContent:"space-between"}}>
+        <button>Discover A.I.</button>
+        <div>
+          <div>Sophisticated</div>
+          <div>Skincare</div>
+        </div>
+        <button>Take test</button>
       </div>
+      <div className="center" style={{height:"5vh",justifyContent:"space-between"}} ></div>
     </>
   );
 }
