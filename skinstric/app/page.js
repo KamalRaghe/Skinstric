6326c47132d7ad9b647939f3bc188d2f78da8374
Kamal <kamal.raghe33@gmail.com>
2026-04-11 17:32:13 -0400

    first commit

--- skinstric/app/page.js
+++ skinstric/app/page.js
@@ -1,19 +1,57 @@
-import Image from "next/image";
-
+"use client" 
+import './globals.css';
+import { GoTriangleRight } from "react-icons/go";
+import { GoTriangleLeft } from "react-icons/go";
+import { useState } from "react";
 export default function Home() {
+const [hovered, setHovered] = useState(null);
+const [hide, setHide] = useState(null);
+
   return (
     <div style={{height:"100vh",padding:"0",margin:"0px"}} >
     <div className="center" style={{justifyContent:"space-between"}} >
-      <div style={{margin:"15px"}}>SKINSTRIC <span style={{color:"grey"}} >[ INTRO ]</span></div>
+      <div style={{margin:"15px"}}>SKINSTRC <span style={{color:"grey"}} >[ INTRO ]</span></div>
       <div><button style={{backgroundColor:"black",color:'white',padding:"10px",marginTop:"15px",fontSize:"12px"}} >Enter Code</button></div>
     </div>
-      <div className="center heading" style={{justifyContent:"space-between",height:"75vh"}}>
-        <button>Discover A.I.</button>
-        <div>
+      <div className="center" style={{justifyContent:"space-between",height:"75vh"}}>
+        <button className="center" 
+          onMouseEnter={() => {setHovered("left");setTimeout(() => setHide("right"), 1000);}}
+          onMouseLeave={() => {setHovered(null);setHide(null)}}>
+          <div className= 'center' 
+          style={{transform:"rotate(45deg)",border:"1px dashed grey", opacity: hovered === "left" ? 0 : 1,
+                  pointerEvents: hovered === "left" ? "none" : "auto",transition: "opacity 1s ease",
+                  display: hide === "left" ? "none" : "flex",width:"300px",height:"300px",position:"relative",right:"175px"}}>
+            <div style={{display:"inline-block",border:"1px solid black",
+                        margin:"15px", position:"relative",left:"40px",bottom:"40px"}} >
+              <div className="center" 
+              style={{scale:"2",transform:"rotate(-45deg)",width:"44px",height:"45px",position:"relative",right:"2px"}}>
+                <GoTriangleLeft></GoTriangleLeft>
+              </div>
+            </div>
+          </div>
+          <span style={{position:"relative",right:"230px"}} >Discover A.I.</span>
+        </button>
+        <div className= 'center column heading ' style={{margin:"0",scale:"1.4"}}>
           <div>Sophisticated</div>
           <div>Skincare</div>
         </div>
-        <button>Take test</button>
+        <button className="center"
+          onMouseEnter={() => {setHovered("right"); setTimeout(() => setHide("left"), 1000);}}
+          onMouseLeave={() => {setHovered(null);setHide(null)}} > 
+          <span style={{position:"relative",left:"215px"}} >Take test</span>
+           <div className='center'
+          style={{transform:"rotate(45deg)",border:"1px dashed grey", opacity: hovered === "right" ? 0 : 1,
+                  pointerEvents: hovered === "left" ? "none" : "auto", transition: "opacity 1s ease",
+                  width:"300px",height:"300px",position:"relative",left:"180px"}}>
+            <div style={{display:"inline-block",border:"1px solid black",
+                        margin:"15px", position:"relative", right:"50px",top:"50px"}} >
+              <div className="center" 
+              style={{scale:"2",transform:"rotate(-45deg)",width:"44px",height:"45px",position:"relative",left:"2px"}}>
+                <GoTriangleRight></GoTriangleRight>
+              </div>
+            </div>
+          </div>
+        </button>
       </div>
       <div className="center" style={{justifyContent:"start", width:"316px",height:"72px",margin:"15px"}} >
         SKINSTRIC DEVELOPED AN A.I. THAT CREATES A HIGHLY-PERSONALIZED ROUTINE TAILORED TO WHAT YOUR SKIN NEEDS.
