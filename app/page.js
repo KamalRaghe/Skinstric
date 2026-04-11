33d4f9c14bd3ffec9b8cbe6b0ec97f4b48ce7055
Kamal <kamal.raghe33@gmail.com>
2026-04-10 22:45:25 -0400

    first commit

--- app/page.js
+++ app/page.js
@@ -12,19 +12,28 @@ export default function Home() {
       <div><button style={{backgroundColor:"black",color:'white',padding:"10px",marginTop:"15px",fontSize:"12px"}} >Enter Code</button></div>
     </div>
       <div className="center heading" style={{justifyContent:"space-between",height:"75vh"}}>
-        <button>
-          <div style={{transform:"rotate(90deg)",border:"1px solid black",display:"inline-block"}} >
-            <div className="" style={{transform:"rotate(90deg)",border:"1px solid black"}}>
-              <GoTriangleRight></GoTriangleRight>
+        <button className="center" >
+          <div style={{transform:"rotate(45deg)",display:"inline-block",border:"1px solid black",margin:"15px"}} >
+            <div className="center" 
+            style={{scale:"2",transform:"rotate(-45deg)",width:"44px",height:"45px",position:"relative",right:"2px"}}>
+              <GoTriangleLeft></GoTriangleLeft>
             </div>
           </div>
           Discover A.I.
         </button>
-        <div>
+        <div className="center column">
           <div>Sophisticated</div>
           <div>Skincare</div>
         </div>
-        <button>Take test</button>
+        <button className="center" > 
+          Take test
+          <div style={{transform:"rotate(45deg)",display:"inline-block",border:"1px solid black",margin:"20px"}} >
+            <div className="center" 
+            style={{scale:"2",transform:"rotate(-45deg)",width:"44px",height:"45px",position:"relative",left:"2px",bottom:"2px"}}>
+              <GoTriangleRight></GoTriangleRight>
+            </div>
+          </div>
+        </button>
       </div>
       <div className="center" style={{justifyContent:"start", width:"316px",height:"72px",margin:"15px"}} >
         SKINSTRIC DEVELOPED AN A.I. THAT CREATES A HIGHLY-PERSONALIZED ROUTINE TAILORED TO WHAT YOUR SKIN NEEDS.
