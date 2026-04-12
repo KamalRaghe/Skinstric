ad157e910a52b37968fe9e00662ba3a4856b1886
Kamal <kamal.raghe33@gmail.com>
2026-04-12 16:38:58 -0400

    first commit

--- app/testing.js
+++ app/testing.js
@@ -1,29 +1,7 @@
 "use client" 
 import './globals.css';
-import { GoTriangleRight } from "react-icons/go";
-import { GoTriangleLeft } from "react-icons/go";
 import { useEffect, useState } from "react";
 export default function Home() {
-const [hideLeft, setHideLeft] = useState(null);
-const [hideRight, setHideRight] = useState(null);
-const [mainLeft, setMainLeft] = useState('center');
-const [mainRight, setMainRight] = useState('center')
-const [loaded, setLoaded] = useState(false)
-const [moveCenter, setMoveCenter] = useState('');
-
-useEffect(()=>{
- if(!loaded){
-  const timer = setTimeout(() => {
-    setHideRight(false);
-    setHideLeft(false);
-  }, 600);
-  setTimeout(() => {
-    setMainLeft('center')
-    setMainRight('center')
-  },2000)
-   return () => clearTimeout(timer);
- } 
-})
 
   return (
     <div style={{height:"100vh",padding:"0",margin:"0px",overflow:"hidden"}} >
