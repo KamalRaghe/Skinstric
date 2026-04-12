cfa050a93d0adc53681c78db6c26e397dce6314d
Kamal <kamal.raghe33@gmail.com>
2026-04-12 12:49:25 -0400

    first commit

--- app/page.js
+++ app/page.js
@@ -26,7 +26,7 @@ useEffect(()=>{
 })
 
   return (
-    <div style={{height:"100vh",padding:"0",margin:"0px"}} >
+    <div style={{height:"100vh",padding:"0",margin:"0px",overflow:"hidden"}} >
     <div className="center" style={{justifyContent:"space-between"}} >
       <div style={{margin:"15px"}}>SKINSTRC <span style={{color:"grey"}} >[ INTRO ]</span></div>
       <div><button style={{backgroundColor:"black",color:'white',padding:"10px",marginTop:"15px",fontSize:"12px"}} >Enter Code</button></div>
