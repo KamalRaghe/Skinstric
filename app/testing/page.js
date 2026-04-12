136d7d9e5b3110be6ac668ceee702190ca53267d
Kamal <kamal.raghe33@gmail.com>
2026-04-12 17:35:08 -0400

    first commit

--- app/testing/page.js
+++ app/testing/page.js
@@ -9,26 +9,27 @@ export default function Home() {
       <div><button style={{backgroundColor:"black",color:'white',padding:"10px",marginTop:"15px",fontSize:"12px"}} >Enter Code</button></div>
     </div>
     <div style={{fontWeight:"bold",position:"relative",left:"20px"}} >To START ANALYSIS</div>
-      <div className="center" style={{width:"100%",height:"70vh",transform:"rotate(45deg)"}} >
-        <div className="center" style={{width:"370px",height:"370px", border:"2px dashed rgba(128,128,128,0.3)"}} >
-            <div className="center" style={{width:"340px",height:"340px",border:"2px dashed rgba(128,128,128,0.6)"}} >
-                <div className="center" style={{width:"300px",height:"300px",border:"2px dashed rgba(128,128,128)"}}>
-                    <input placeholder="Introduce Yourself" 
-                    className="center"
-                    style={{
-                        padding:"0px",
-                        textAlign:"center",
-                        transform:"rotate(-45deg)",
-                        border:'none',
-                        borderBottom:"2px solid grey",
-                        color: 'black',
-                        fontSize:"40px",
-                        width:"350px"
-                    }} ></input>
-                </div>
+      <div className="center" style={{width:"100%", height:"70vh"}}>
+        <div className="box outer center">
+            <div className="box middle center">
+            <div className="box inner center"></div>
+            </div>
             </div>
         </div>
-      </div>
+        <div className="center" >
+                    <input
+                    placeholder="Introduce Yourself"
+                    style={{
+                    textAlign: "center",
+                    bottom:"300px",
+                    border: "none",
+                    borderBottom: "2px solid grey",
+                    fontSize: "40px",
+                    width: "350px",
+                    position:"fixed"
+                }}
+                />
+        </div>
     </div>
   );
 }
