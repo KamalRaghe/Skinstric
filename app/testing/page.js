9b686180dc882ff7ae245a59c28b65315c08f02d
Kamal <kamal.raghe33@gmail.com>
2026-04-13 20:04:11 -0400

    first commit

--- app/testing/page.js
+++ app/testing/page.js
@@ -17,10 +17,25 @@ const router = useRouter()
   return (
     <div style={{height:"100vh",padding:"0",margin:"0px",overflow:"hidden"}} >
     <div className="center" style={{justifyContent:"space-between",margin:"8px 0px"}} >
-      <div style={{margin:"15px", fontSize:'12px'}}><span style={{fontWeight:"bold",fontSize:"10px",margin:"4px"}} >SKINSTRC</span> <span style={{color:"grey"}} >[ INTRO ]</span></div>
-      <div><button style={{backgroundColor:"black",color:'white',padding:"10px",marginTop:"15px",fontSize:"12px"}} >Enter Code</button></div>
+      <div style={{margin:"15px", fontSize:'12px'}}>
+        <span style={{fontWeight:"bold",fontSize:"10px",margin:"4px"}} >SKINSTRC</span> 
+        <span style={{color:"grey"}} >[ INTRO ]</span></div>
+      <div>
+        <button 
+        style={{
+            backgroundColor:"black",
+            color:'white',
+            padding:"10px",
+            marginTop:"15px",
+            fontSize:"12px"}} >Enter Code</button></div>
     </div>
-    <div style={{fontWeight:"bold",position:"relative",left:"22px",fontSize:"12px"}} >To START ANALYSIS</div>
+    <div style={{
+        fontWeight:"bold",
+        position:"relative",
+        left:"22px",
+        fontSize:"12px"}} >
+            To START ANALYSIS
+        </div>
       <div className="center" style={{width:"100%", height:"70vh"}}>
         <div className="box outer center">
             <div className="box middle center">
