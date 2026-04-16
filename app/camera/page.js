97860bb08a6184c93b2a407c451f4e9d142d22cf
Kamal <kamal.raghe33@gmail.com>
2026-04-16 18:28:21 -0400

    first commit

--- app/camera/page.js
+++ app/camera/page.js
@@ -2,13 +2,13 @@ import { MdOutlineCamera } from "react-icons/md";
 export default function Cam(){
     return(
         <div>
-            <div className="center" style={{height:"95vh",overflowY:"hidden"}} >
+            <div className="center" style={{height:"95vh"}} >
                 <div style={{ width: "450px", height: "450px", border: "1px dashed grey", position: "relative" }}>
                     <div className="center"  style={{ width: "100%", height: "100%", position: "relative",overflow:"scroll" }}>
             
                     <div style={{
-                         width: "80%",
-                         height: "80%",
+                         width: "70%",
+                         height: "70%",
                          border: "2px dashed grey",
                          position: "absolute",
                          animation: "rotate 50s linear infinite",
@@ -16,10 +16,6 @@ export default function Cam(){
                     }}/>
             
                     <MdOutlineCamera style={{
-                        position: "absolute",
-                        top: "65%",
-                        left: "65%",
-                        transform: "translate(-50%, -50%)",
                         width: "45px",
                         height: "45px",
                         borderRadius: "50%",
@@ -28,50 +24,6 @@ export default function Cam(){
                         scale: "2.8"
                     }}
                     />
-            
-                    {/* ✅ TOP RIGHT CONNECTOR */}
-                     <div style={{
-                         position: "absolute",
-                         top: "65%",
-                         left: "65%",
-                         transform: "translate(-50%, -50%)"
-                    }}>
-                        {/* line */}
-                        <div style={{
-                        position: "absolute",
-                        width: "95px",
-                        height: "2px",
-                        background: "#6f6f6f",
-                        transform: "rotate(-41deg)",
-                        transformOrigin: "0% 50%",
-                        left: "5px",   // starts at circle edge
-                        top: "-85px"
-                        }}/>
-                         <div style={{
-                         position: 'relative',
-                         top:"-150px",
-                         left:"75px",
-                         width: "4px",
-                         height: "4px",
-                         border: "1px solid black",
-                         borderRadius: "50%",
-                        }}/>
-                        {/* text */}
-                        <div style={{
-                         position: "absolute",
-                         width:"150px",
-                         left: "85px",
-                         top: "-162px",
-                         fontSize: "13px",
-                         color: "#444",
-                         lineHeight: "1.3",
-                         letterSpacing: "0.3px"
-                         }}>
-                         ALLOW A.I.<br/>
-                        TO SCAN YOUR FACE
-                        </div>
-                    </div>
-            
                     </div>
                 </div>
                  </div>
