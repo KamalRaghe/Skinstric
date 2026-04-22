9d480df7d842ea21d1da890c6151da1d0cd62b53
Kamal <kamal.raghe33@gmail.com>
2026-04-22 17:44:53 -0400

    first commit

--- app/select/page.js
+++ app/select/page.js
@@ -71,10 +71,14 @@ export default function Select() {
         style={{
           width:"100%",
           height:"100%",
-          overflow:'hidden',
+          overflowX:'hidden',
+          position:"fixed",
+          left:"50%",
+          top:"50%",
+          transform:"translate(-50%,-50%)"
         }}
       >
-        <div className=" center" style={{ width: "330px", height: "330px",transform:"rotate(45deg)" }}>
+        <div className=" center" style={{width: "300px", height: "300px",transform:"rotate(45deg)" }}>
           
           {/* 🔥 BIG BORDER OVERLAY (this is what animates) */}
           <div className="center"
