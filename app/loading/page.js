a992e15ae9a8faeed86e1dfeea173f5b3c9bfe99
Kamal <kamal.raghe33@gmail.com>
2026-04-28 01:15:13 -0400

    first commit

---
+++ app/loading/page.js
@@ -0,0 +1,42 @@
+"use client";
+
+export default function LoadingPage() {
+  return (
+    <div className="" >
+         <div className="center" style={{ justifyContent: "space-between", margin: "8px 0px" }}>
+        <div style={{ margin: "15px", fontSize: "12px" }}>
+          <span style={{ fontWeight: "bold", fontSize: "10px", margin: "4px" }}>
+            SKINSTRC
+          </span>
+          <span style={{ color: "grey" }}>[ INTRO ]</span>
+        </div>
+        <button style={{ backgroundColor: "black", color: "white", padding: "10px", marginTop: "15px", fontSize: "12px" }}>
+          Enter Code
+        </button>
+      </div>
+        <div className="center" style={{ height: "82vh" }} >
+      <div className="center" style={{ position: "relative" }}>
+
+        {/* ROTATING DIAMONDS */}
+        <div className="outer center" style={{ position: "absolute" }}>
+          <div className="middle center">
+            <div className="inner center" />
+          </div>
+        </div>
+
+        {/* STATIC TEXT (not inside rotating divs) */}
+        <div className="center column" style={{ width:"500px",position: "absolute" }}>
+         
+            PREPARING YOUR ANALYSIS...
+
+          <div className="dots">
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+        </div>
+      </div>
+      </div>
+    </div>
+  );
+}
--- app/result.js
+++ app/result.js
@@ -1,4 +1,4 @@
-c"use client";
+"use client";
 import { useRef, useState } from "react";
 
 export default function ScanPage() {
